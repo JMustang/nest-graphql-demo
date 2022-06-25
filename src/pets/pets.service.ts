ed77421240cef80e4dd3f6d784547aff4d6d4280
Junior <eecfredes@gmail.com>
2022-06-25 12:49:44 -0300

    feat: Pet database Entity

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Pet } from './pet.entity';
 
 @Injectable()
 export class PetsService {
+  constructor(@InjectRepository(Pet) private petRepository: Repository<Pet>) { }
+
   async findAll(): Promise<Pet[]> {
     const pet = new Pet();
     pet.id = 1;
