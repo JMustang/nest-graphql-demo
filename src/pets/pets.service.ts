0ce3b65f3ecf729165335a50fceb450d012ef12f
Junior <eecfredes@gmail.com>
2022-06-27 17:56:13 -0300

    feat: createPet Implementation

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -5,10 +5,19 @@ import { Pet } from './pet.entity';
 
 @Injectable()
 export class PetsService {
-  constructor(@InjectRepository(Pet) private petRepository: Repository<Pet>) { }
+  constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>) { }
+
+
+
+  // newPet = new Pet(); new.name = input.name
+  createPet(createPetInput): Promise<Pet> {
+    const newPet = this.petsRepository.create(createPetInput);
+
+    return this.petsRepository.save(newPet); // insert
+  }
 
   async findAll(): Promise<Pet[]> {
-    return this.petRepository.find() // [SQL] => SELECT * from Pet
+    return this.petsRepository.find() // [SQL] => SELECT * from Pet
 
   }
 }
