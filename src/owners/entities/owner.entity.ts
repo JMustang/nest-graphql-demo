72f2d8752f5dd0a2b946d9edc043e72272fa23bc
Junior <eecfredes@gmail.com>
2022-06-30 20:21:38 -0300

    feat: Owners database entity

--- src/owners/entities/owner.entity.ts
+++ src/owners/entities/owner.entity.ts
@@ -1,7 +1,15 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Owner {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field(type => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
 }
